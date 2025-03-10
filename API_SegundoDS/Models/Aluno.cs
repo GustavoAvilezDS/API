d92bf9d4cd9cb3512ae8d303b56651d93deca508
GustavoAvilezDS <gustavoavilez373@gmail.com>
2025-03-10 09:07:05 -0300

    feat: classes Escola, Professor e Aluno

---
+++ API_SegundoDS/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace API_SegundoDS.Models
+{
+
+    public class Aluno
+
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? RMA { get; set; }
+        public string? PAI { get; set; }
+        public string? MAE { get; set; }
+        public ESCOLA ESCOLA { get; set; }
+        public Professor Professor { get; set; }
+
+
+
+
+    }
+}
+
---
+++ API_SegundoDS/Models/PROFESSOR.cs
@@ -0,0 +1,11 @@
+﻿module PROFESSOR
+{ 
+      public int Id { get; set; }
+public string? Nome { get; set; }
+public string? RMA { get; set; }
+public string? PAI { get; set; }
+public string? MAE { get; set; }
+public ESCOLA ESCOLA { get; set; }
+public PROFESSOR PROFESSOR { get; set; }
+
+}
